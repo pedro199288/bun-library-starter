5f952859b71ae9ea43f0cd0c191884f631156fd9
Pedro Monteagudo <pedromonteagudo_@hotmail.com>
2024-01-16 12:38:54 +0100

    fix publish dist content

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { getInitialLetter } from './some-module';
+
 export function greet(name: string): string {
-  console.log(`Hello, ${name}!`);
+  console.log(
+    `Hello, ${name}! your initial letter is ${getInitialLetter(name)}`,
+  );
   return `Hello, ${name}!`;
 }
 
